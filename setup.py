1f9a42c3d2cdb5195857251cc6e6aaa05f37b357
Pedro Dias <pedrodias.miguel@gmail.com>
2020-07-24 17:19:07 +0100

    Add setup.py to the repository

---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+Python installation script
+"""
+from setuptools import find_packages, setup
+
+
+setup(
+    name="spike_removal",
+    version="0.0.1",
+    license="MIT License",
+    description="Tool used to remove spikes from polygons in geospatial data",
+    long_description="""
+        Tool used to remove spikes from polygons in geospatial data
+    """,
+    author="Pedro Dias",
+    author_email="pedrodias.miguel@gmail.com",
+    url="https://github.com/pmdias/spike_removal",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Scientific/Engineering :: GIS",
+        "Topic :: Utilities"
+    ],
+    project_urls={
+        "Issue Tracker": "https://github.com/pmdias/spike_removal/issues",
+    },
+    keywords=[
+
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "geopandas>=0.8.1",
+    ],
+    entry_points={
+        "console_scripts": []
+    },
+)
